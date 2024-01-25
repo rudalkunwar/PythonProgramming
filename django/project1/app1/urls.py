da8a1196e1aaa4334cbc6fb5706105bf3411eb95
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-25 11:41:21 +0545

    python

--- django/project1/app1/urls.py
+++ django/project1/app1/urls.py
@@ -1,1 +1,4 @@
 from django.urls import path,include
+from .import views
+
+urlpatterns = [path('',views.myfunctioncall,name='index'),path('about',views.about,name='about')]
--- django/project1/app1/views.py
+++ django/project1/app1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
+
+def myfunctioncall(request):
+    return HttpResponse("Starting to learn Django")
+
+def about(request):
+    return HttpResponse('This is about page')
